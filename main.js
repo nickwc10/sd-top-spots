03922fb78db797df67555c363fa0879535888a44
Nicholas Christensen <pixeltasim@gmail.com>
2026-03-04 13:41:08 -0500

    Update.

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 $.getJSON("data.json", function (data) {
     $.each(data, function (index, spot) {
-        $("table").append(
+        $("#spots-body").append(
             `<tr>
                 <td>${spot.name}</td>
                 <td>${spot.description}</td>
-                <td><a id="map-btn" href="https://www.google.com/maps?q=${spot.location[0]}},${spot.location[1]}">Open in Google Maps</a></td>
+                <td><a id="map-btn" href="https://www.google.com/maps?q=${spot.location[0]},${spot.location[1]}">Open in Google Maps</a></td>
             </tr>`
         );
     });
-});
+}); 

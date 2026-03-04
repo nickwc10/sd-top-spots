ef68eafa5348ba94f2f3dbf95a292a3ae1015c64
Nicholas Christensen <pixeltasim@gmail.com>
2026-03-04 13:34:35 -0500

    Version 0.2

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-$(document).ready(function () {
-    // write your code here
-
+$.getJSON("data.json", function (data) {
+    $.each(data, function (index, spot) {
+        $("table").append(
+            `<tr>
+                <td>${spot.name}</td>
+                <td>${spot.description}</td>
+                <td><a id="map-btn" href="https://www.google.com/maps?q=${spot.location[0]}},${spot.location[1]}">Open in Google Maps</a></td>
+            </tr>`
+        );
+    });
 });
